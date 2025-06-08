bdfc4085e648e59f5f63c7d67ec344ca2d57ee79
joseph <josephdaher25@hotmail.com>
2025-06-08 16:22:07 -0400

    changes 5

--- Service-website/src/main.jsx
+++ Service-website/src/main.jsx
@@ -9,7 +9,7 @@ import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter  basename="/Driven/Service-website/">
+    <BrowserRouter  basename="/Driven">
       <App />
     </BrowserRouter>
   </React.StrictMode>

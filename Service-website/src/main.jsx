5aeaf95225d1bfc49a4d94017835fcb36334b3c1
joseph <josephdaher25@hotmail.com>
2025-06-08 16:56:32 -0400

    back to browserrouter

--- Service-website/src/main.jsx
+++ Service-website/src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
@@ -9,8 +9,8 @@ import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HashRouter  basename="/Driven">
+    <BrowserRouter  basename="/Driven">
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
